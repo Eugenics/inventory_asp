05a4cc451f06b80309ae6cc067475ef958d7ca1b
Alex Pasternak <orphen@sibmail.com>
2019-07-26 10:57:23 +0700

    first comm

--- inventory_dot_core/Areas/Admin/Model/User.cs
+++ inventory_dot_core/Areas/Admin/Model/User.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace inventory_dot_core.Areas.Admin.Model
 {
-    public partial class User
+    public partial class User :IdentityUser
     {
+        public string Role { get; set; }
     }
 }
